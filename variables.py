3f167be47ae8b9f06fb3c5c8818bcb480b34f71e
zarraga555 <zarraga555@gmail.com>
2020-09-23 14:55:34 -0400

    variables

---
+++ variables.py
@@ -0,0 +1,24 @@
+name = "Fazt"
+print(name)
+print(10+101)
+
+x= 100
+#casos sensitive
+#Las variables son diferentes
+book = "Digital Fortres"
+Book = "Digital Fortres"
+
+#Otra manera de declara variables
+y, libro = 100, 'I robot'
+
+#variables convenciones
+book_name= "I robot" #Snake Case
+bookName = "Digital fortess" #Camel Case
+BookName = "asasdasdasdad" #Pascal Case
+
+#Cuando se usa mayusculas la variable, este valor no debe cambiar
+PI = 3.1416
+
+#variables reasignadas 
+ejemplo = "I robot"
+ejemplo = 121213
