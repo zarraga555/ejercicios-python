e78912a6b409a6580cc17b04266928fe4befb06f
zarraga555 <zarraga555@gmail.com>
2020-09-23 14:54:57 -0400

    datatype

---
+++ datatype.py
@@ -0,0 +1,43 @@
+#String
+print("Hello World")
+print('Hello world')
+print('''Helloe world''')
+print("""hello world""")
+
+#Type muestra el tipo de dato a imprimir
+#print(type("Hello world"))
+
+# el simbolo + es para concatenar
+print('bye'+'world')
+
+#Numeros
+
+#Integer Enteros
+print(30)
+
+#Float
+print(30.2)
+
+#Boolean Booleano
+# True
+# False
+
+#List Lista
+# [10, 20, 30, 55]
+# ['Hello', 'Bye', 'Adios']
+# [10, 'Hello', True, 10.1]
+# []
+
+#Tuples
+(10, 20, 30, 55)
+()
+
+#Dictorionies
+print(type({
+    "name":"Ryan",
+    "apellido":"Ray",
+    "surname":"Fazt"
+}))
+
+#Typedate None
+#None
