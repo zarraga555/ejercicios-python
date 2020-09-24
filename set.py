c6fe8934d6cf87596865ea89bd1f341a444b2839
zarraga555 <zarraga555@gmail.com>
2020-09-23 22:52:14 -0400

    set

---
+++ set.py
@@ -0,0 +1,12 @@
+colors = {'Red', 'Green', 'Blue'}
+print(colors)
+print('Red' in colors)
+colors.add('Violet')
+print(colors)
+
+colors.remove('Red')
+
+# colors.clear()
+# del colors
+
+print(colors)
