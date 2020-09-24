ca828e87f90d22d1f333895cd985b039da29a71a
zarraga555 <zarraga555@gmail.com>
2020-09-23 22:51:23 -0400

    functions

---
+++ functions.py
@@ -0,0 +1,16 @@
+# def hello():
+#   print('Hello World')
+
+# hello()
+
+def hello(name):
+  print('Hello ' + name)
+
+hello('Juan')
+hello('Alberto')
+hello('Ryan')
+
+def add(a,b):
+  return a + b
+
+print(add(10, 30))
