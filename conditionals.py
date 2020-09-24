dd40146fe99b219335feb526c3f37d276ab78cf7
zarraga555 <zarraga555@gmail.com>
2020-09-23 22:48:09 -0400

    conditionals

---
+++ conditionals.py
@@ -0,0 +1,43 @@
+# x = 10
+# if x < 30:
+#   print('x is less than 30')
+
+# if x > 30:
+#   print('x is greater than 30')
+
+# if x == 30:
+#   print('x is 30')
+
+# if x < 20:
+#   print('x is less than 20')
+# else:
+#   print('x is greater than 20')
+
+# color = 'blue'
+
+# if color == 'red':
+#   print('the color is red')
+# elif color == 'blue':
+#   print('the color is blue')
+# else:
+#   print('any color')
+
+# name = 'Jhon'
+# lastname = 'Carters'
+
+# if name == 'Jhon':
+#   if lastname == 'Carter':
+#     print('name and lastname is true')
+#   else:
+#     print('You are not Jhon Carter')
+# else:
+#   print('You are not Jhon')
+x = 1
+y = 2
+
+if x > 2 and  x < 10:
+  print('x is greater than two and less than or equal to ten')
+if x >2 or x <= 20:
+  print('x is greater than two or less or equal two twenty')
+if(not(x==y)):
+  print('x is not equal y')
