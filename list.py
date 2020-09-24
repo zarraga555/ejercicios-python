e99646eac9ced873bd3cadc193611e7b53648b7e
zarraga555 <zarraga555@gmail.com>
2020-09-23 22:51:42 -0400

    list

--- list.py
+++ list.py
@@ -19,3 +19,26 @@ print('green' in colors)
 print(colors)
 colors[1] = 'Yellow'
 print(colors)
+
+colors.append('violet')
+colors.extend(('violet', 'green'))
+
+colors.insert(1, 'violet')
+colors.insert(-1, 'blue')
+colors.insert(len(colors), 'violet')
+
+#Elimina un artibuto con ese nonbre
+colors.remove('green')
+
+print(colors)
+#Elimina el ultimp de la lista o asignado la posicion
+colors.pop()
+#Limpia la lista
+# colors.clear()
+
+#Ordena la lista z-a
+colors.sort(reverse=True)
+
+#cuenta la cantidad del valor asignado existe
+print(colors.count('red'))
+print(colors)
